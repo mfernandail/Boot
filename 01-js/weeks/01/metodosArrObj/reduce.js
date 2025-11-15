cbcfb395f72d58f444be47e228e0dfdb17d7e770
mfernandail <maria.ibanez.leiva@gmail.com>
2025-11-14 22:33:21 -0300

    Refactor reduce.js to calculate student averages and filter by grade, including enhancements for better readability and structure.

--- 01-js/weeks/01/metodosArrObj/reduce.js
+++ 01-js/weeks/01/metodosArrObj/reduce.js
@@ -6,12 +6,57 @@ const estudiantes = [
 ]
 
 // TODO: Calcula el promedio de cada estudiante con map y reduce
-const promedios = estudiantes.map((estudiante) => {
-  const sumaNotas = estudiante.notas.reduce((acum, nota) => acum + nota, 0)
+const notas = estudiantes.map((estudiante) => {
+  const notas = estudiante.notas.reduce((acc, el) => acc + el, 0)
   return {
     nombre: estudiante.nombre,
-    promedio: sumaNotas / estudiante.notas.length,
+    promedio: notas / estudiante.notas.length,
   }
 })
+console.log(notas)
 
-console.log(promedios)
+// TODO: Filtra estudiantes con promedio >= 8
+const filtraProm = notas.filter((nota) => nota.promedio >= 8)
+console.log(filtraProm)
+
+// TODO: Encuentra el estudiante con mejor promedio
+const mejorProm = notas.reduce(
+  (acc, el) => {
+    if (acc.promedio < el.promedio) {
+      acc.nombre = el.nombre
+      acc.promedio = el.promedio
+    }
+    return acc
+  },
+  { nombre: '', promedio: 0 }
+)
+console.log(mejorProm)
+
+// TODO: Crea un objeto con Object.entries que agrupe por edad
+const agruparPorEdad = estudiantes.reduce((acc, estudiante) => {
+  const edad = estudiante.edad
+  if (!acc[edad]) {
+    acc[edad] = []
+  }
+
+  acc[edad].push(estudiante)
+  return acc
+}, {})
+
+agruparPorEdad
+
+const palabras = ['Ana', 'Alberto', 'Beatriz', 'Carlos', 'Carla']
+
+const agrupar = palabras.reduce((acc, el) => {
+  const letra = el.charAt(0)
+
+  if (!acc[letra]) {
+    acc[letra] = []
+  }
+
+  acc[letra].push(el)
+
+  return acc
+}, {})
+
+console.log(agrupar)
